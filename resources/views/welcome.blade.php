d3c4a83792060bf501ea0637cd26361e4e55dae0
B-len <mary.belencatorce@gmail.com>
2020-09-28 22:16:00 +0200

    Primer comit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!Doctype html>
+<html lang="es-ES">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Documento</title>
+</head>
+<body>
+    <nav>
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Mi bloc de notas</a>
+
+        </div>
+    </nav>
+    <div id="example"></div>
+    <script src="/js/app.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+</body>
+</html>
